031a399a69dc0bacedee2b0a298c8e8ecab8e01d
alexandre <alexandres@sicara.com>
2019-01-28 09:40:43 +0100

    Add babel to override antd css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Button from 'antd/lib/button';
+import { Button } from 'antd';
 import './App.css';
+
 
 class App extends Component {
   render() {
---
+++ src/App.style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { Button as AntButton } from 'antd';
+
+export const Button = styled(AntButton)`
+  border: none;
+`;
