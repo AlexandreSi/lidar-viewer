4523709065433116627f5cc2dd9e22c57c1a7ee9
alexandre <alexandres@sicara.com>
2019-02-03 19:25:11 +0100

    Animate with react-transition-group

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-import { Title, Button } from './App.style';
+import { Title } from './App.style';
+import { Button } from 'antd';
 import './App.css';
+import { CSSTransition } from 'react-transition-group'
 
 
 class App extends Component {
+  state = {
+    showTitle: false,
+    showButton: true,
+  }
+
+  componentDidMount() {
+    this.handleClick()
+    this.setState({ showButton: false })
+  }
+
+  handleClick = () => {
+    this.setState({
+      showTitle: !this.state.showTitle,
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Title>Welcome to LiDAR-viewer</Title>
-        <Button type="primary">Continue</Button>
+        <CSSTransition
+          in={this.state.showTitle}
+          classNames="title"
+          timeout={1200}
+          onEntered={() => this.setState({ showButton: true })}
+        >
+          <Title>Welcome to LiDAR-viewer</Title>
+        </CSSTransition>
+        <CSSTransition
+          in={this.state.showButton}
+          classNames="continue-button"
+          timeout={1500}
+        >
+          <Button type="primary" onClick={this.handleClick}>Continue</Button>
+        </CSSTransition>
       </div>
     );
   }
--- src/App.style.js
+++ src/App.style.js
@@ -1,20 +1,9 @@
 import styled from 'styled-components';
-import { Button as AntButton } from 'antd';
-
 import { style } from 'theme/style';
-
-const fadeDuration = "1.2s";
 
 
 export const Title = styled.h1`
   color: ${style['@primary-color']};
   font-size: 40px;
   font-family: ${style.fonts.primary};
-  animation: fade-down ${fadeDuration} ease-out forwards;
-`;
-
-
-export const Button = styled(AntButton)`
-  opacity: 0;
-  animation: fade ${fadeDuration} ease-out 1s forwards;
 `;
