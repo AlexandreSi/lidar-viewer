7ead90cddd3f6ed9c6b919f9cc50a8bcdda16c42
alexandre <alexandres@sicara.com>
2019-02-21 12:40:28 +0100

    Handle icons for last step

--- src/components/Steps/Steps.component.js
+++ src/components/Steps/Steps.component.js
@@ -5,14 +5,25 @@ import { ProgressContainer } from './Steps.style';
 
 
 const Steps = (props) => {
-  const currentStep = props.fileRead ? 1 : 0;
+  const currentStep = props.colorsLoaded
+    ? 2
+    : props.fileRead ? 1 : 0;
 
   return (
     <ProgressContainer>
       <AntDesignSteps current={currentStep}>
-        <AntDesignSteps.Step title="Reading .LAS file" icon={currentStep === 0 ? <Icon type="loading" /> : <Icon type="file-add" />} />
-        <AntDesignSteps.Step title="Loading colors" icon={currentStep === 1 ? <Icon type="loading" /> : <Icon type="bg-colors" />} />
-        <AntDesignSteps.Step title="Ready to explore" icon={currentStep === 2 ? <Icon type="loading" /> : <Icon type="unlock" />} />
+        <AntDesignSteps.Step
+          title="Reading .LAS file"
+          icon={currentStep === 0 ? <Icon type="loading" /> : <Icon type="file-add" />}
+        />
+        <AntDesignSteps.Step
+          title="Loading colors"
+          icon={currentStep === 1 ? <Icon type="loading" /> : <Icon type="bg-colors" />}
+        />
+        <AntDesignSteps.Step
+          title="Ready to explore"
+          icon={currentStep === 2 ? <Icon type="unlock" /> : <Icon type="lock" />}
+        />
       </AntDesignSteps>
     </ProgressContainer>
   )
